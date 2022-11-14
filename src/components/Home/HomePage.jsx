5fc503f12696a23dfd6f8c277fbf72a4fb636f2a
Truong Tan Dat <truongtandat1409@gmail.com>
2022-11-14 11:57:59 +0700

    fix ui v2

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material'
+import { Box, Button, Card, CardActionArea, CardContent, CardMedia, Container, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from '@mui/material'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
@@ -8,17 +8,40 @@ const HomePage = () => {
 
     // const bookList = useSelector((state) => state.books.books?.allBooks);
     const petList = useSelector((state) => state.pet.pet?.allPets);
-
+    const petCateList = useSelector((state) => state.category.category?.allCates);
+    const [cate, setCate] = React.useState('');
     const dispatch = useDispatch()
 
-    const nav = useNavigate()
     useEffect(() => {
         getAllPets(dispatch)
     }, [dispatch])
 
 
+    const renderCategory = () => {
+        return petCateList?.map((value) => {
+            return <MenuItem value={value.name}>{value.name}</MenuItem>;
+        });
+    };
+
+    const handleChange = (event) => {
+        setCate(event.target.value);
+    };
     return (
         <Container sx={{ mt: 5 }}>
+            <Box sx={{ minWidth: 120, textAlign: "right" }}>
+                <FormControl sx={{ width: "6rem" }}>
+                    <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={cate}
+                        label="Category"
+                        onChange={handleChange}
+                    >
+                        {renderCategory()}
+                    </Select>
+                </FormControl>
+            </Box>
             <Typography
                 variant="h4"
                 sx={{ fontWeight: "bold", textAlign: "center", mb: 5 }}
@@ -30,7 +53,9 @@ const HomePage = () => {
                 spacing={{ xs: 2, md: 3 }}
                 columns={{ xs: 4, sm: 8, md: 12 }}
             >
-                {petList?.map((pet) => (
+
+
+                {petList.filter(p => p.cate_name === cate)?.map((pet) => (
                     <Grid item xs={2} sm={4} md={4} key={pet.id}>
                         <Card sx={{ maxWidth: 345, minHeight: 400 }}>
                             <CardActionArea>
@@ -84,3 +109,38 @@ const HomePage = () => {
 }
 
 export default HomePage
+
+
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+// import Select from '@mui/material/Select';
+
+// export default function BasicSelect() {
+//   const [age, setAge] = React.useState('');
+
+//   const handleChange = (event) => {
+//     setAge(event.target.value);
+//   };
+
+//   return (
+//     <Box sx={{ minWidth: 120 }}>
+//       <FormControl fullWidth>
+//         <InputLabel id="demo-simple-select-label">Age</InputLabel>
+//         <Select
+//           labelId="demo-simple-select-label"
+//           id="demo-simple-select"
+//           value={age}
+//           label="Age"
+//           onChange={handleChange}
+//         >
+//           <MenuItem value={10}>Ten</MenuItem>
+//           <MenuItem value={20}>Twenty</MenuItem>
+//           <MenuItem value={30}>Thirty</MenuItem>
+//         </Select>
+//       </FormControl>
+//     </Box>
+//   );
+// }
